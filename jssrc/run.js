a7bf73104ffd732c894968f6d7399eb02a9f2c59
Laendasill <panador@gmail.com>
2015-06-23 10:18:23 +0200

    qwe

--- jssrc/run.js
+++ jssrc/run.js
@@ -6,25 +6,36 @@ var interval;
 function run(speed=1000){
   var u15 = document.getElementById("u15");
   var u12 = document.getElementById("u12");
+  var format = document.getElementById("binary");
+  var format2 = document.getElementById("hex");
   var i = 0;
-  var q = function(i,r=false){
-    var r = [1,1,1,1];
-    r[i] = 0;
+  var hexes = ["11011110","11101101","01111011","10110111"];
+  var q = function(i,r=false,u12=false){
+    var ret = hexes[i].toString(16);
     if(r===true){
-      r.reverse();
+      ret = "0x" + parseInt(hexes[i],2).toString(16);
     }
-    var ret = r.join('');
+    if(u12 === true){
+      
+    }
+
     return ret;
   };
   interval = setInterval(function(){
+    var form = false;
+    if(format.checked == true){
+      form = false;
+    } else if(format2.checked == true){
+      form = true;
+    }
     if(i == 4){
       i = 0;
     }
-    u15.innerHTML = `U15 = ${q(i)}`;
+    u15.innerHTML = `U15 = ${q(i,form)}`;
     var el = test.checkRow(i);
     if(!isNaN(el)){
       console.log(el);
-      var str = `U12 = ${q(el)}`;
+      var str = `U12 = ${q(el,false)}`;
       u12.innerHTML =   str;
     }else{
       console.log("no click detected "  );
